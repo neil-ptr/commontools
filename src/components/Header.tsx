dbb6bb44f6ead4b6a33c4735f84dac03e6565fd1
Neil Viloria <neilcviloria@gmail.com>
2025-07-27 23:54:17 -0600

    center header items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ import ToggleTheme from "./ToggleTheme";
 const Header = () => {
   return (
     <header>
-      <nav className="grid grid-cols-3 p-6 sticky top-0 bg-background z-10">
+      <nav className="grid grid-cols-3 p-6 sticky top-0 bg-background z-10 items-center">
         <div className="justify-self-start">
           <Link href="/" className="font-bold">
             CommonTools
